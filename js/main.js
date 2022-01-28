579d53409a878e9cb58bc1e3bbdb2a6b66db5ce8
winterandrea <andrecasero@gmail.com>
2022-01-28 12:10:22 +0100

    r

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-const url = '../docs/pdf.pdf';
+const url = "../docs/pdf.pdf";
 
 let pdfDoc = null,
   pageNum = 1,
   pageIsRendering = false,
   pageNumIsPending = null;
 
-const scale = 1.5,
-  canvas = document.querySelector('#pdf-render'),
-  ctx = canvas.getContext('2d');
+const scale = 0.6,
+  canvas = document.querySelector("#pdf-render"),
+  ctx = canvas.getContext("2d");
 
 // Render the page
-const renderPage = num => {
+const renderPage = (num) => {
   pageIsRendering = true;
 
   // Get page
-  pdfDoc.getPage(num).then(page => {
+  pdfDoc.getPage(num).then((page) => {
     // Set scale
     const viewport = page.getViewport({ scale });
     canvas.height = viewport.height;
@@ -22,7 +22,7 @@ const renderPage = num => {
 
     const renderCtx = {
       canvasContext: ctx,
-      viewport
+      viewport,
     };
 
     page.render(renderCtx).promise.then(() => {
@@ -35,12 +35,12 @@ const renderPage = num => {
     });
 
     // Output current page
-    document.querySelector('#page-num').textContent = num;
+    document.querySelector("#page-num").textContent = num;
   });
 };
 
 // Check for pages rendering
-const queueRenderPage = num => {
+const queueRenderPage = (num) => {
   if (pageIsRendering) {
     pageNumIsPending = num;
   } else {
@@ -69,23 +69,23 @@ const showNextPage = () => {
 // Get Document
 pdfjsLib
   .getDocument(url)
-  .promise.then(pdfDoc_ => {
+  .promise.then((pdfDoc_) => {
     pdfDoc = pdfDoc_;
 
-    document.querySelector('#page-count').textContent = pdfDoc.numPages;
+    document.querySelector("#page-count").textContent = pdfDoc.numPages;
 
     renderPage(pageNum);
   })
-  .catch(err => {
+  .catch((err) => {
     // Display error
-    const div = document.createElement('div');
-    div.className = 'error';
+    const div = document.createElement("div");
+    div.className = "error";
     div.appendChild(document.createTextNode(err.message));
-    document.querySelector('body').insertBefore(div, canvas);
+    document.querySelector("body").insertBefore(div, canvas);
     // Remove top bar
-    document.querySelector('.top-bar').style.display = 'none';
+    document.querySelector(".top-bar").style.display = "none";
   });
 
 // Button Events
-document.querySelector('#prev-page').addEventListener('click', showPrevPage);
-document.querySelector('#next-page').addEventListener('click', showNextPage);
+document.querySelector("#prev-page").addEventListener("click", showPrevPage);
+document.querySelector("#next-page").addEventListener("click", showNextPage);
