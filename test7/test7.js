a21e0de6c0b122b40fca2581e7b32d746815fea6
winterandrea <andrecasero@gmail.com>
2022-01-28 12:45:08 +0100

    rivista

---
+++ test7/test7.js
@@ -0,0 +1,12 @@
+// init Isotope
+var $grid = $(".grid").isotope({
+  itemSelector: ".book-subject",
+  layoutMode: "masonry",
+});
+
+$(".filters").on("click", "a", function (e) {
+  var bookSubject = $(this).attr("data-filter");
+  $grid.isotope({ filter: bookSubject });
+  $(".filters > a").removeClass("selected");
+  $(e.currentTarget).addClass("selected");
+});
